ef1c44425a6a8a6e85a6d8d936987fd56bf6a71e
farhani2001 <farhan.anjum2001@gmail.com>
2025-06-22 18:16:39 +0000

    tutorial finished

---
+++ frontend/src/services/api.js
@@ -0,0 +1,16 @@
+const API_KEY = "ca8523867b9d86c0e33b29cc307b506f";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(
+        `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`
+    );
+    const data = await response.json();
+    return data.results;
+};
