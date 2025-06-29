1f3dd683257ddf847b2877b37b2361c34c4e19dd
farhani2001 <farhan.anjum2001@gmail.com>
2025-06-29 13:08:00 +0000

    Add Watch Later functionality to watchlater page jsx and styles; slight update to Favorites page

--- frontend/src/pages/Watch-later.jsx
+++ frontend/src/pages/Watch-later.jsx
@@ -1,8 +1,29 @@
 import { useMovieContext } from "../contexts/MovieContext";
 import MovieCard from "../components/MovieCard";
+import "../css/WatchLater.css";
 
 function WatchLater() {
-    
+    const { watchLater} = useMovieContext();
+
+    if (watchLater.length > 0) {
+        return (
+            <div className="watch-later">
+                <h2>What will you watch next...</h2>
+                <div className="movie-grid">
+                    {watchLater.map((movie) => (
+                        <MovieCard movie={movie} key={movie.id} />
+                    ))}
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className="watch-later-empty">
+            <h2>No movie you wanna watch later</h2>
+            <p>Start adding movies you wanna watch later</p>
+        </div>
+    );
 }
 
 export default WatchLater;
