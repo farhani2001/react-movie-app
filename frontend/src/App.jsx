5e83a33923c25041665067af1cdc5c139b18091c
farhani2001 <farhan.anjum2001@gmail.com>
2025-07-20 11:47:32 +0000

    stuck on fetching node backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,36 @@ import WatchLater from './pages/Watch-later';
 import {Routes, Route} from "react-router-dom";
 import NavBar from './components/NavBar';
 import {MovieProvider} from "./contexts/MovieContext";
+import React, {useState, useEffect} from "react";
 
 function App() {
+  
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(console.log("fetching")).then(
+      response => response.json()
+    ).then(
+        data => {
+          setBackendData(data)
+          console.log("data fetched", data)
+        }
+      )
+    },[])
+    
+    console.log("backendData:", backendData);
   
   return (
     <MovieProvider>
       <div>
+        {(typeof backendData.users === 'undefined') ? (
+          <p>loading</p>
+        ) : (
+          backendData.users.map((user,i) => (
+            <p key={i}> {user}</p>
+          ))
+        )}
+        
         <NavBar/>
         <main className="main-content">
           <Routes>
